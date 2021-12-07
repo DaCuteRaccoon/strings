b8dc2dafeee0a34f014ebddeba09141fd2ac2707
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2021-12-06 17:40:49 -0800

    Create Strings++.js

---
+++ bin/Strings++.js
@@ -0,0 +1,17 @@
+(function(){ return typeof void 0 })()
+
+undef = undefined
+// Type undef == undefined in the console, and you get true. How neat is that? You don't have to type all of undefined.
+
+NULL = null
+nil = null
+Nil = null
+NIL = null
+none = null
+None = null
+NONE = null
+
+True = true
+TRUE = true
+False = false
+FALSE = false
