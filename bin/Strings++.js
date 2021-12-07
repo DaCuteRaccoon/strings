f5b799c231e904e8238f701ce2c53bafeac5a550
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2021-12-06 18:00:12 -0800

    Update Strings++.js

--- bin/Strings++.js
+++ bin/Strings++.js
@@ -1,5 +1,9 @@
+/*
 (function(){ return typeof void 0 })()
+*/
 
+
+(function(){
 undef = undefined
 // Type undef == undefined in the console, and you get true. How neat is that? You don't have to type all of undefined.
 
@@ -15,3 +19,11 @@ True = true
 TRUE = true
 False = false
 FALSE = false
+
+return void 0
+})()
+
+
+// If you are wondering why this is in a function, it's because if it werent, the console would say the last value in the list. For example, if the last line was NULL = null, it would say null in the console. With the function I can make it say what I want.
+
+// tl;dr
